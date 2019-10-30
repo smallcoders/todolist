552adc84bfa0dbe1beefd40de49b848db0fc6551
zjma5 <1137145490@qq.com>
2019-10-30 16:41:45 +0800

    修改入口文件依赖

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import Todolist from './Todolist';
 
 ReactDOM.render(<Todolist />, document.getElementById('root'));
