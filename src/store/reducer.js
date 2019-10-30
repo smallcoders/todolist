1a6b49ba1dc3f83186ad154327fbf8f9fffef2af
zjma5 <1137145490@qq.com>
2019-10-30 16:20:53 +0800

    添加注释

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -2,7 +2,7 @@ const defaultState = {
   inputValue: '123',
   list: [1, 2]
 };
-
+// reducer 只能接收store，不能更改store
 export default (state = defaultState, action) => {
   if (action.type === 'change_input_value') {
     const newState = JSON.parse(JSON.stringify(state));
